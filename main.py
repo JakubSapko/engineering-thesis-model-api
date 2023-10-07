dc755870b9b174827f64d1eec10250c618d8cad7
JakubSapko <jakub.sapko1@gmail.com>
2023-10-07 14:04:26 +0200

    add: script connection

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 
 app = FastAPI()
 
 
 @app.post("/")
-async def root():
+async def root(request: Request):
+    print("test")
+    result = await request.body()
+    print(result)
     return {"message": "Hello World"}
